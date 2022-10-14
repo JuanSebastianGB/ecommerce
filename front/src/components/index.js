79741efa9fbc5aca9c95b0b0258700d8d5efec2e
unknown <3393@holbertonstudents.com>
2022-10-13 20:57:38 -0500

    Cratign slider component

--- front/src/components/index.js
+++ front/src/components/index.js
@@ -3,3 +3,4 @@ export * from './header';
 export * from './card';
 export * from './loader';
 export * from './showLink';
+export * from './slider';
---
+++ front/src/components/slider/Slider.jsx
@@ -0,0 +1,42 @@
+import { Fragment, useState } from 'react';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+import { sliderData } from './mock.slider.data';
+import './Slider.scss';
+
+const lengthData = sliderData.length;
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const showNextSlide = () =>
+    setCurrentSlide(currentSlide < lengthData - 1 ? currentSlide + 1 : 0);
+  const showPrevSlide = () =>
+    setCurrentSlide(currentSlide === 0 ? lengthData - 1 : currentSlide - 1);
+  return (
+    <div>
+      <AiOutlineArrowLeft className="arrow prev" onClick={showPrevSlide} />
+      <AiOutlineArrowRight className="arrow next" onClick={showNextSlide} />
+      {sliderData.map(({ desc, heading, image }, index) => (
+        <div
+          className={index === currentSlide ? 'slide current' : 'slide'}
+          key={index}
+        >
+          {index === currentSlide && (
+            <Fragment>
+              <img width="300" src={image} alt="image" />
+              <div className="content">
+                <h2>{heading}</h2>
+                <p>{desc}</p>
+                <hr />
+                <a href="#product" className="--btn --btn-primary">
+                  Shop Now
+                </a>
+              </div>
+            </Fragment>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
